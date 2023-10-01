b53c6a815c8181095884d92571e9375530518871
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-30 19:05:06 -0500

    Created build board function.

--- src/board.js
+++ src/board.js
@@ -1,11 +1,12 @@
-const boardSize = 10;
-const board = [];
+function buildBoard(boardSize) {
+    board = [];
 
-for (let i = 0; i != boardSize; i++) {
-    board.push([]);
-    for (let j = 0; j != boardSize; j++) {
-        board[i].push(0);
+    for(let i=0; i!=boardSize; i++){
+        board.push([]);
+        for (let j=0; j!=boardSize;j++){
+            board[i].push(0);
+        }
     }
-}
 
-console.log(board);
+    return board;
+}
