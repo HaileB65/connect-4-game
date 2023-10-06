776fc1164a72dcb7e7ff7764b2bedd06e3a8938d
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-10-05 20:16:31 -0500

    Updated render_board file

--- src/render_board.js
+++ src/render_board.js
@@ -1,21 +1,14 @@
 function buildBoardElement(board){
-
-    // Building the board container element.
     let boardElement = c("table", null, "board");
 
     board.forEach((row) => {
-
-        // Every cell needs to go in a row
         const rowElement = c("tr", null,"board-row");
 
         row.forEach((value) => {
             const cell = c("td", value, "cell");
-
-            // Adding the cell to the row
             rowElement.append(cell);
         })
 
-        // Adding the row to the board
         boardElement.append(rowElement);
     })
     return boardElement;
