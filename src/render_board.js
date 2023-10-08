869d65a3e91563a4cd39fc0315a52e7245f9287e
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-10-08 16:04:10 -0500

    Updated render_board logic to log to console the table cell clicked on.

--- src/render_board.js
+++ src/render_board.js
@@ -1,15 +1,25 @@
 function buildBoardElement(board){
     let boardElement = c("table", null, "board");
 
-    board.forEach((row) => {
+    board.forEach((row, i) => {
         const rowElement = c("tr", null,"board-row");
-
-        row.forEach((value) => {
-            const cell = c("td", value, "cell");
-            rowElement.append(cell);
+        row.forEach((value, j) => {
+            const cell = createCell(value, i, j);
+            rowElement.appendChild(cell);
         })
-
-        boardElement.append(rowElement);
+        boardElement.appendChild(rowElement);
     })
     return boardElement;
+}
+
+function handleBoardClick(row, col) {
+    console.log(`updating row ${row} and column ${col}`)
+}
+
+function createCell(value, row, col){
+    cell = c("td", value, "cell");
+    cell.addEventListener("click", e => {
+        handleBoardClick(row, col);
+    })
+    return cell;
 }
